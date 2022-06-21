0fd92e925d0f6940f2075ee19346c98d3fa75271
sibowen535 <bowen.simon@gmail.com>
2022-06-21 14:31:37 +0100

    file structure

---
+++ code-bandits/src/Libs/index.js
@@ -0,0 +1,35 @@
+/*
+Create an example object
+
+const exObject = {
+    url: "string",
+    title: "string",
+    contentType: [article, video, documentation],
+    searchTopic: [html, css, javascript]
+
+
+};
+
+const anotherObject = {
+    url: "string",
+    title: "string",
+    contentType: {
+        article:false,
+        video:false,
+        doscumentation:false
+    },
+    searchTopic: {
+        html:false,
+        css:false,
+        javascript:false
+    };
+
+}
+
+User will type or past a URL - string
+Will input a title - string
+Choose topic from drop-down - value? 
+Choose type from drop-down - value?
+
+
+*/
