49a3b7798b44288e17e169af443b822c3ec8fde9
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-06-22 23:47:06 +0100

    add grid structure to resource-content-list class

--- code-bandits/src/Components/ResourceCard/ResourceCard.js
+++ code-bandits/src/Components/ResourceCard/ResourceCard.js
@@ -1,11 +1,27 @@
+import { useState } from 'react'
 const ResourceCard = ({ title, url, content_type, topic }) => {
+  const [icon, setIcon] = useState(null);
+
+  const handleType = (content_type) => {
+    if(content_type === 'video') {
+      setIcon('📽')
+    }
+    else if(content_type === 'article') {
+      setIcon('📰')
+    }
+    else {
+      setIcon('📝')
+    }
+    return icon
+  }
+
   return (
     <li>
-      <a href={url} target="_blank">
+      <a href={url} target="_blank" rel='noreferrer'>
         <div class="resource-content-wrapper">
           <div class="resource-header">
             {title}
-            <p>[icon] type</p>
+            <p>{handleType(content_type)} type</p>
           </div>
           <div class="resource-body"></div>
           <div class="resource-content_type">{content_type}</div>
