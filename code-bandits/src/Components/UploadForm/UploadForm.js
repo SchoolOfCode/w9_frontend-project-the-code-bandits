2bf42e886b46ac8d57d0da98c173736e6be2cd46
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-06-22 10:58:30 +0100

    creating plan for uploadform componenent

--- code-bandits/src/Components/UploadForm/UploadForm.js
+++ code-bandits/src/Components/UploadForm/UploadForm.js
@@ -1,3 +1,22 @@
+// import everything we need
+// create functional component
+// Create Form
+// text input with article heading label
+// text input for url
+
+// dropdown for content_type
+// checkbox for topics
+// submit button
+
+// state is form values
+// update state with statefunction with onchange (e.target.value)
+// createResource function
+// on Submit = async function (e)
+// try and catch err handling
+// fetch method with url , request obj with 
+// method : post , Headers: something , body : state
+
+
 const UploadForm = () => {
     return (  );
 }
