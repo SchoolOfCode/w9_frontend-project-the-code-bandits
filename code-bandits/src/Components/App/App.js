d03a3f4763b130021848b7edfc4fb0fd0f0b8f8b
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-06-22 15:13:33 +0100

    changing form to actually communicate with frontend

--- code-bandits/src/Components/App/App.js
+++ code-bandits/src/Components/App/App.js
@@ -1,9 +1,11 @@
 import "../../index.css";
+import UploadForm from "../UploadForm/UploadForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <UploadForm />
     </div>
   );
 }
