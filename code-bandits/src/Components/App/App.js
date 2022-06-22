6baa3f13819d4f12295b46966814d043eda3ec06
piratejas <piratejas@gmail.com>
2022-06-22 15:55:08 +0100

    debugged fetch from API

--- code-bandits/src/Components/App/App.js
+++ code-bandits/src/Components/App/App.js
@@ -1,7 +1,25 @@
 import "../../index.css";
 import Header from "../Header/Header";
+import { useEffect, useState } from "react";
+import fetchFromAPI from "../../services/fetchFromAPI";
 
 function App() {
+  const [resourceList, setResourceList] = useState([]);
+
+  const getResources = async () => {
+    try {
+      const data = fetchFromAPI("http://localhost:3001/resources");
+      setResourceList(data);
+      console.log(resourceList);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getResources();
+  }, []);
+
   return (
     <div className="App">
       <Header />
--- code-bandits/src/services/fetchFromAPI.js
+++ code-bandits/src/services/fetchFromAPI.js
@@ -1,6 +1,7 @@
 const fetchFromAPI = async (url) => {
   const response = await fetch(url);
   const fetchData = await response.json();
+  console.log(fetchData);
   return fetchData;
 };
 
