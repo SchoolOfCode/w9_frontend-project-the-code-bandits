eeb22c08d8d41f0c317b35026af94f983f38fb88
piratejas <piratejas@gmail.com>
2022-06-22 13:20:35 +0100

    added initial resource data library

---
+++ code-bandits/src/Libs/data.js
@@ -0,0 +1,38 @@
+export const initialResources = [
+  {
+    title: "Git diff Command – How to Compare Changes in Your Code",
+    url: "https://www.freecodecamp.org/news/git-diff-command/",
+    content_type: "article",
+    topic: "Git",
+  },
+  {
+    title: "SQL Injection",
+    url: "https://www.w3schools.com/sql/sql_injection.asp",
+    content_type: "tutorial",
+    topic: "SQL",
+  },
+  {
+    title: "Zelda: Breath of the Wild with CSS",
+    url: "https://mastery.games/build/zelda/",
+    content_type: "video",
+    topic: "CSS",
+  },
+  {
+    title: "Fetching data using inbuilt fetch API",
+    url: "https://www.codingdeft.com/posts/react-fetch-data-api/#fetching-data-using-inbuilt-fetch-api",
+    content_type: "article",
+    topic: "React",
+  },
+  {
+    title: "How to Use Git and Git Workflows – a Practical Guide",
+    url: "https://www.freecodecamp.org/news/practical-git-and-git-workflows/",
+    content_type: "article",
+    topic: "Git",
+  },
+  {
+    title: "Common mistakes with React Testing Library",
+    url: "https://kentcdodds.com/blog/common-mistakes-with-react-testing-library",
+    content_type: "article",
+    topic: "React",
+  },
+];
