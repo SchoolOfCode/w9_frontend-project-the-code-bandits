61a23789d016d70642559fb614cb76fa9ec94b10
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-06-22 11:57:22 +0100

    add more to plan in FilterSection

--- code-bandits/src/Components/FilterContainer/FilterContainer.js
+++ code-bandits/src/Components/FilterContainer/FilterContainer.js
@@ -3,11 +3,11 @@ import FilterSection from "../FilterSection/FilterSection";
 const FilterContainer = () => {
     return ( 
       <aside class="filter">
-      <div class="filter-wrapper">
-      <h3 class="filter-subheading">Filter by</h3>
-      <FilterSection />
-      <FilterSection />
-      </div>
+        <div class="filter-wrapper">
+        <h3 class="filter-subheading">Filter by</h3>
+        <FilterSection />
+        <FilterSection />
+        </div>
       </aside>
      );
 }
--- code-bandits/src/Components/FilterSection/FilterSection.js
+++ code-bandits/src/Components/FilterSection/FilterSection.js
@@ -1,8 +1,43 @@
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+
 const FilterSection = () => {
     return ( 
       <fieldset class="filter-content">
           <legend class="filter-content-subheading">Content type</legend>
-        
+          {/* map through array of prop items (possibly need a method for dealing with objects)
+          for each unique property we will create a FilterCheckbox instant
+          
+          Find a way to store the user choice
+
+            initial state {
+              article: false
+              video: false
+              documentation: false
+            }
+
+            if all props are false then display all (no filter)
+            if a prop is true then this will be read by DisplayContainer
+
+            Pira - DisplayContainer is always rendering an array of resources
+            onChange of the FilterCheckbox state. This will create a new mutable array (not changing original resource array) that is then rendered by DisplayContainer
+
+            User stories
+
+            User must be able to filter by BOTH content and topic
+
+            So maybe we need two states to make the design clearer.
+            These states will be in the FilterSection component.
+
+
+
+          change state variable to reflect user filter choices
+
+          present 
+          
+          */
+          }
+          <FilterCheckbox />
+
       </fieldset>
      );
 }
