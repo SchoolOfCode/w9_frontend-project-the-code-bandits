8d095904e8c97e88b97d9d747dfce6940f52e6f2
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-06-22 11:25:51 +0100

    add structure to FilterCheckbox component

--- code-bandits/src/Components/FilterCheckbox/FilterCheckbox.js
+++ code-bandits/src/Components/FilterCheckbox/FilterCheckbox.js
@@ -1,5 +1,10 @@
 const FilterCheckbox = () => {
-    return (  );
+    return ( 
+            <div class="content-item">
+              <input type="checkbox" name="content" id="content-documentation" />
+              <label class="filter-content-list-item" for="content-documentation">Documentation</label>
+          </div>
+     );
 }
  
 export default FilterCheckbox;
