c2e89c4b4453b60baef7cbf554b10e595c2dcb2a
piratejas <piratejas@gmail.com>
2022-06-23 10:09:51 +0100

    solved infinite loop

--- code-bandits/src/Components/ResourceCard/ResourceCard.js
+++ code-bandits/src/Components/ResourceCard/ResourceCard.js
@@ -1,31 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react";
 const ResourceCard = ({ title, url, content_type, topic }) => {
   const [icon, setIcon] = useState(null);
 
   const handleType = (content_type) => {
-    if(content_type === 'video') {
-      setIcon('📽')
+    if (content_type === "video") {
+      setIcon("📽");
+    } else if (content_type === "article") {
+      setIcon("📰");
+    } else {
+      setIcon("📝");
     }
-    else if(content_type === 'article') {
-      setIcon('📰')
-    }
-    else {
-      setIcon('📝')
-    }
-    return icon
-  }
+    return icon;
+  };
 
   return (
     <li>
-      <a href={url} target="_blank" rel='noreferrer'>
+      <a href={url} target="_blank" rel="noreferrer">
         <div className="resource-content-wrapper">
           <div className="resource-header">
             {title}
-            <p>{handleType(content_type)} type</p>
+            <p>
+              {() => {
+                handleType(content_type);
+              }}
+              type
+            </p>
           </div>
           <div className="resource-body">
-            <h3 className='resource-title'>{title}</h3>
-            <p className='resource-description'>Click on the <a href="https://www.freecodecamp.org/news/git-diff-command/" target="_blank" rel="noreferrer">freecodecamp link on git diff commands</a> for more information</p>
+            <h3 className="resource-title">{title}</h3>
+            <p className="resource-description">
+              Click on the{" "}
+              <a
+                href="https://www.freecodecamp.org/news/git-diff-command/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                freecodecamp link on git diff commands
+              </a>{" "}
+              for more information
+            </p>
           </div>
           <div className="resource-content_type">{content_type}</div>
           <div className="resource-footer">{topic}</div>
