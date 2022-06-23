c5028404c01b7b790394187c11240ae2ae318711
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-06-23 15:10:58 +0100

    changing styling of add resource button in header

--- code-bandits/src/Components/Header/Header.js
+++ code-bandits/src/Components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = ({ handleShow }) => {
     <header className="header">
       <div className="header-content-wrapper">
         <h1 className="header-title">Resource manager</h1>
-        <Button type="header-button" onClick={handleShow}>
+        <Button className="header-add-resource-button" onClick={handleShow}>
           Add resource
         </Button>
         <div className="header-search-resources">
