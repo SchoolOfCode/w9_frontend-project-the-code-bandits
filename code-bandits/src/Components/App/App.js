c0a74aea7d09de7024e04e824e33d6d3a799d2d4
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-06-28 15:35:34 +0100

    Updated comments to App.js and FilterSection.js

--- code-bandits/src/Components/App/App.js
+++ code-bandits/src/Components/App/App.js
@@ -4,10 +4,14 @@ import { useEffect, useState } from "react";
 
 import fetchFromAPI from "../../services/fetchFromAPI";
 
+// Components
 import UploadFormPopUp from "../UploadForm/UploadFormPopUp";
 import Header from "../Header/Header";
 import DisplayContainer from "../DisplayContainer/DisplayContainer";
 import FilterSection from "../FilterSection/FilterSection";
+
+// Uses fetch from database on pages initial load to update resourceList state
+// Updates showForm state to true or false (this shows the UploadFormPopUp component)
 
 function App() {
   const [resourceList, setResourceList] = useState([]);
@@ -16,6 +20,7 @@ function App() {
   const handleClose = () => setShowForm(false);
   const handleShow = () => setShowForm(true);
 
+// fetchFromAPI comes from services folder
   const getResources = async () => {
     try {
       const data = await fetchFromAPI("http://localhost:3001/resources");
@@ -26,33 +31,22 @@ function App() {
     }
   };
 
+// fetch on initial load
   useEffect(() => {
     getResources();
   }, []);
 
-  //function that will take an object (typeChecked) that will map through resourceList and check value of content_type and compare to typeChecked object if true
-  //forIn loop to check properties of an object
-
-  //check typeChecked object for any property: true
-  //take the name of that property store in a variable as a string
-  //if that variable string === value of content_type then push to filteredList
-  //then update state of resourcesList to filteredList
-
-  /*{
-    article: true,
-    video: false,
-    documentation: false,
-  }*/
-
+// State to handle filtered resources
   const [filteredList, setFilteredList] = useState(resourceList);
   console.log(filteredList);
 
+// Immediate re-render of filteredList (displayed resources) on update
   useEffect(() => {
     setFilteredList(resourceList);
   }, [resourceList]);
 
+// Returns reources that match object properties
   function handleFilter(obj) {
-    console.log(obj);
     const filtered = resourceList.filter((item) => {
       if (!obj.article && !obj.docs && !obj.video) {
         return item;
@@ -66,7 +60,7 @@ function App() {
     });
     console.log("filtered", filtered);
     setFilteredList(filtered);
-
+    
     console.log(filteredList);
   }
 
